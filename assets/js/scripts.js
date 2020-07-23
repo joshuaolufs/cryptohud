d93707b2bb7f79ca41487bbf913b1065ddcd13ea
joshuaolufs <olufsjoshua@gmail.com>
2020-07-23 07:20:52 -0500

    added all crypto's on binance dynamically

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,85 +1,31 @@
-new TradingView.widget({
-    "autosize": "true",
-    "height": 200,
-    "symbol": "BINANCE:LINKUSDT",
-    "interval": "1",
-    "timezone": "Etc/UTC",
-    "theme": "dark",
-    "style": "8",
-    "locale": "en",
-    "toolbar_bg": "#f1f3f6",
-    "enable_publishing": false,
-    "hide_top_toolbar": true,
-    "save_image": false,
-    "container_id": "tradingview_db1"
-});
-new TradingView.widget({
-    "autosize": "true",
-    "symbol": "BINANCE:XRPUSDT",
-    "interval": "1",
-    "timezone": "Etc/UTC",
-    "theme": "dark",
-    "style": "8",
-    "locale": "en",
-    "toolbar_bg": "#f1f3f6",
-    "enable_publishing": false,
-    "hide_top_toolbar": true,
-    "save_image": false,
-    "container_id": "tradingview_db2"
-});
-new TradingView.widget({
-    "autosize": "true",
-    "symbol": "BINANCE:KNCUSDT",
-    "interval": "1",
-    "timezone": "Etc/UTC",
-    "theme": "dark",
-    "style": "8",
-    "locale": "en",
-    "toolbar_bg": "#f1f3f6",
-    "enable_publishing": false,
-    "hide_top_toolbar": true,
-    "save_image": false,
-    "container_id": "tradingview_db3"
-});
-new TradingView.widget({
-    "autosize": "true",
-    "symbol": "BINANCE:BTCUSDT",
-    "interval": "1",
-    "timezone": "Etc/UTC",
-    "theme": "dark",
-    "style": "8",
-    "locale": "en",
-    "toolbar_bg": "#f1f3f6",
-    "enable_publishing": false,
-    "hide_top_toolbar": true,
-    "save_image": false,
-    "container_id": "tradingview_db4"
-});
-new TradingView.widget({
-    "autosize": "true",
-    "symbol": "BINANCE:XTZUSDT",
-    "interval": "1",
-    "timezone": "Etc/UTC",
-    "theme": "dark",
-    "style": "8",
-    "locale": "en",
-    "toolbar_bg": "#f1f3f6",
-    "enable_publishing": false,
-    "hide_top_toolbar": true,
-    "save_image": false,
-    "container_id": "tradingview_db5"
-});
-new TradingView.widget({
-    "autosize": "true",
-    "symbol": "BINANCE:ADAUSDT",
-    "interval": "1",
-    "timezone": "Etc/UTC",
-    "theme": "dark",
-    "style": "8",
-    "locale": "en",
-    "toolbar_bg": "#f1f3f6",
-    "enable_publishing": false,
-    "hide_top_toolbar": true,
-    "save_image": false,
-    "container_id": "tradingview_db6"
-});
+var coins = ["ADA", "ALGO", "ATOM", "BAT", "BCH", "BNB", "BTC", "BUSD", "DASH", "DOGE", "ENJ", "EOS", "ETC", "ETH", "HBAR", "ICX", "IOTA", "KNC", "LINK", "LTC", "MATIC", "NANO", "NEO", "OMG", "ONT", "QTUM", "REP", "RVN", "USDT", "VET", "WAVES", "XLM", "XRP", "XTZ", "ZEC", "ZIL", "ZRX"];
+
+var chartsContainer = document.getElementById("grid-container");
+for (var i = 0; i < coins.length; i++) {
+    // generate the html for the chart
+    var htmlOutput = "<div class='chart-info'></div><div class='tradingview-widget-container'><div id='tradingview_db" + i + "' class='chart'></div></div>";
+    var outputElement = document.createElement('div');
+    outputElement.innerHTML = htmlOutput;
+    outputElement.classList.add("grid-item");
+    chartsContainer.appendChild(outputElement);
+
+    // populate it with a chart
+    var currentCoin = "BINANCE:" + coins[i] + "USDT";
+    var currentId = "tradingview_db" + i;
+    new TradingView.widget({
+        "autosize": "true",
+        "height": 200,
+        "symbol": currentCoin,
+        "interval": "1",
+        "timezone": "Etc/UTC",
+        "theme": "dark",
+        "style": "1",
+        "locale": "en",
+        "toolbar_bg": "#f1f3f6",
+        "enable_publishing": false,
+        "hide_legend": true,
+        "allow_symbol_change": true,
+        "save_image": false,
+        "container_id": currentId
+    });
+}
